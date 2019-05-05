c7ec3b566815455151dd449ad7f17cb30b045c8a
RaulCapellanFernandez <raulcape.7@gmail.com>
2019-05-05 20:08:44 +0200

    Ejemplo para cada clase

--- Peluqueria/src/main/java/es/peluqueria/data/Application.java
+++ Peluqueria/src/main/java/es/peluqueria/data/Application.java
@@ -12,65 +12,72 @@ import es.peluqueria.persistence.PersistVentas;
 public class Application {
 
 	public static void main(String[] args) {
-		
-		
+		PersistEmpleados persistE = new PersistEmpleados();
 		PersistAlmacen persistA = new PersistAlmacen();
+		PersistProducto persistP = new PersistProducto();
+		PersistVentas persistV  = new PersistVentas();
+		
+		//Guardar cada clase
+		Empleados empleado = new Empleados();
+		empleado.setDni("71471266P");
+		empleado.setNombre("Maria");
+		empleado.setApellidos("Hernandez");
+		empleado.setFechaContra(new Date());
+		empleado.setEmail("mcapef00@estudiantes.unileon.es");
+		empleado.setCalle("Universidad de Leon");
+		empleado.setPiso("12");
+		empleado.setCiudad("Madrid");
+		empleado.setProvincia("Sanse");
+		empleado.setCodPostal("24010");
 		
 		Almacen almacen = new Almacen();
-		almacen.setNombre("H&S Limon");
+		almacen.setNombre("Tinte");
 		almacen.setCantidad(3);
 		
 		persistA.save(almacen);
+		persistE.save(empleado);
 		
-		
-		PersistEmpleados persistE = new PersistEmpleados();
 	
-		Empleados empleado = new Empleados();
-		empleado.setDni("722255221");
-		empleado.setNombre("Raul");
-		empleado.setApellidos("Capellan");
-		empleado.setFechaContra(new Date());
-		empleado.setEmail("raulcape.7@gmail.com");
-		empleado.setCalle("Astorga 16");
-		empleado.setPiso("4ºA");
-		empleado.setCiudad("Leon");
-		empleado.setProvincia("Leon");
-		empleado.setCodPostal("24009");
-		
-		
-		//persistE.save(empleado);
-		
-		PersistProducto persistP = new PersistProducto();
-		
 		Productos producto = new Productos();
-		producto.setNombre("Corte de pelo");
-		producto.setPrecio(12);
+		producto.setNombre("Pintar el pelo");
+		producto.setPrecio(24);
 		
 		List<Almacen>listaAlmacen = persistA.recuperar();
-		
 		producto.setAlmacen(listaAlmacen.get(0));
 		
+		persistP.save(producto);
+		
 		List<Productos>listaProductos = persistP.recuperar();
-		listaProductos.get(0).setNombre("La vida es muy dura");
-		
-		persistP.update(listaProductos.get(0));
-		
-		//persistP.save(producto);
-		//List<Productos>listaProductos = persistP.recuperar();
-		//persistP.remove(listaProductos.get(0));
-		
-		/*List<Empleados>listaEmpleados = persistE.recuperar();
-		List<Productos>listaProductos = persistP.recuperar();
-		
-		PersistVentas persistV  = new PersistVentas();
+		List<Empleados>listaEmpleados = persistE.recuperar();
 		
 		Ventas venta = new Ventas();
-		venta.setTotalVenta(124);
+		venta.setTotalVenta(24);
 		venta.setFechaVenta(new Date());
 		venta.setEmpleado(listaEmpleados.get(0));
 		venta.setProducto(listaProductos.get(0));
 		
-		persistV.save(venta);*/
+		persistV.save(venta);
 		
+		
+		//Actualizar cada clase
+		empleado.setNombre("Teresa");
+		
+		almacen.setNombre("Tinte rojo");
+		
+		producto.setNombre("Cortar el pelo");
+		
+		venta.setTotalVenta(999);
+		
+		persistE.update(empleado);
+		persistA.update(almacen);
+		persistP.update(producto);
+		persistV.update(venta);
+		
+		
+		//Borrar cada clase
+		persistV.remove(venta);
+		persistP.remove(producto);
+		persistE.remove(empleado);
+		persistA.remove(almacen);
 	}
 }
